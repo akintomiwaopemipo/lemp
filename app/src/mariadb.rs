ef6a7a3127ed9577854200ba8b32acbec07b33e0
Akintomiwa Opemipo <ibnakintomiwa@gmail.com>
2025-05-01 18:04:34 +0100

    Install expect

--- app/src/mariadb.rs
+++ app/src/mariadb.rs
@@ -14,6 +14,8 @@ impl Mariadb{
 
         shell_exec("sudo apt install dos2unix -y");
 
+        shell_exec("sudo apt install expect -y");
+
         file_put_contents("mysql_secure.sh", include_str!("../../templates/bash/mysql_secure.sh"));
 
         shell_exec(&format!(r#"dos2unix mysql_secure.sh && chmod +x mysql_secure.sh && ./mysql_secure.sh "{}" && rm -rf mysql_secure.sh"#,root_password));
