1a4f4bb4ba6cad3eb6cbd85c83192054016a3901
Akintomiwa Opemipo <ibnakintomiwa@gmail.com>
2025-05-01 20:17:45 +0100

    Show output of installation ; Open port 7070

--- app/src/phpmyadmin.rs
+++ app/src/phpmyadmin.rs
@@ -11,7 +11,7 @@ impl Phpmyadmin{
 
         let lastest_phpmyadmin_version = "5.2.2";
 
-        shell_exec(&format!("wget files.phpmyadmin.net/phpMyAdmin/{lastest_phpmyadmin_version}/phpMyAdmin-{lastest_phpmyadmin_version}-all-languages.zip -q && mv phpMyAdmin-{lastest_phpmyadmin_version}-all-languages.zip /usr/share/phpMyAdmin-{lastest_phpmyadmin_version}-all-languages.zip && unzip -qq /usr/share/phpMyAdmin-{lastest_phpmyadmin_version}-all-languages.zip -d /usr/share && mv /usr/share/phpMyAdmin-{lastest_phpmyadmin_version}-all-languages /usr/share/phpmyadmin  && rm -rf /usr/share/phpMyAdmin-{lastest_phpmyadmin_version}-all-languages.zip"));
+        shell_exec(&format!("wget files.phpmyadmin.net/phpMyAdmin/{lastest_phpmyadmin_version}/phpMyAdmin-{lastest_phpmyadmin_version}-all-languages.zip && mv phpMyAdmin-{lastest_phpmyadmin_version}-all-languages.zip /usr/share/phpMyAdmin-{lastest_phpmyadmin_version}-all-languages.zip && unzip /usr/share/phpMyAdmin-{lastest_phpmyadmin_version}-all-languages.zip -d /usr/share && mv /usr/share/phpMyAdmin-{lastest_phpmyadmin_version}-all-languages /usr/share/phpmyadmin  && rm -rf /usr/share/phpMyAdmin-{lastest_phpmyadmin_version}-all-languages.zip"));
 
 
         file_put_contents("phpmyadmin.conf", &format!(include_str!("../../templates/phpmyadmin/phpmyadmin.conf")));
@@ -19,6 +19,8 @@ impl Phpmyadmin{
         shell_exec("mv phpmyadmin.conf /etc/nginx/sites-available/phpmyadmin.conf");
 
         shell_exec("sudo ln -s /etc/nginx/sites-available/phpmyadmin.conf /etc/nginx/sites-enabled/");
+
+        shell_exec("sudo ufw allow 7070");
 
         shell_exec("sudo systemctl restart nginx");
         
