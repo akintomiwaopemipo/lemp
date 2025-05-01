d9a4bf4f840231c57ef6626bece14cc7679643f1
Akintomiwa Opemipo <ibnakintomiwa@gmail.com>
2025-05-01 16:54:34 +0100

    Command requires sudo

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ cargo build --release
 echo
 
 if [ -f "target/release/$APP_NAME" ]; then
-    rm -rf "/usr/local/bin/$APP_NAME"
+    sudo rm -rf "/usr/local/bin/$APP_NAME"
     sudo cp "target/release/$APP_NAME" "/usr/local/bin"
     echo "Copied to /usr/local/bin"
 else
