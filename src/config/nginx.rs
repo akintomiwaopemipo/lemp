0a203c356d6cb3cba32eb2efa0534f16f28b2a2b
Akintomiwa Opemipo <ibnakintomiwa@gmail.com>
2025-05-01 15:16:48 +0100

    Replace apache2 config append with nginx

--- src/config/nginx.rs
+++ src/config/nginx.rs
@@ -6,13 +6,9 @@ pub struct Args;
 
 pub fn action(_args: Args){
         
-    util::add_new_line(include_str!("../../templates/apache/config-append.conf"), "/etc/apache2/apache2.conf");
+    util::add_new_line(include_str!("../../templates/nginx/config-append.conf"), "/etc/nginx/nginx.conf");
 
-    util::shell_exec("sudo a2ensite 000-default.conf");
-
-    util::shell_exec("sudo a2enmod rewrite");
-
-    util::shell_exec("sudo systemctl restart apache2");
+    util::shell_exec("sudo systemctl restart nginx");
 
     println!("Restarted apache service");
 }
