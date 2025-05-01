f3b8cd10a992d0f2fe172b087e164f69b3e671fc
Akintomiwa Opemipo <ibnakintomiwa@gmail.com>
2025-05-01 16:50:24 +0100

    Install phpmyadmin ; Better output format for installation

--- src/install/lemp.rs
+++ src/install/lemp.rs
@@ -1,4 +1,4 @@
-use app::{nginx::Nginx, mariadb::Mariadb, php::PHP};
+use app::{nginx::Nginx, mariadb::Mariadb, php::PHP, phpmyadmin::Phpmyadmin};
 use util::read_and_confirm_password;
 use crate::install;
 
@@ -11,12 +11,34 @@ pub fn action(args: install::Args){
 
     println!();
     
+    println!("Installing nginx");
+    println!();
     Nginx::install();
+    println!();
+    println!();
+    println!();
 
+
+    println!("Installing Mariadb");
+    println!();
     Mariadb::install(&root_password);
+    println!();
+    println!();
+    println!();
 
+
+    println!("Installing PHP");
+    println!();
     PHP::install();
+    println!();
+    println!();
+    println!();
 
+
+    println!("Installing PhpMyAdmin");
+    println!();
+    Phpmyadmin::install();
+    println!();
     println!();
 
 }
