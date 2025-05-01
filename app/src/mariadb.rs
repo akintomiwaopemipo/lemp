b4c862606afa6fd19880873cc9d24e92bfa22f42
Akintomiwa Opemipo <ibnakintomiwa@gmail.com>
2025-05-01 18:06:14 +0100

    Better use of format!

--- app/src/mariadb.rs
+++ app/src/mariadb.rs
@@ -18,9 +18,9 @@ impl Mariadb{
 
         file_put_contents("mysql_secure.sh", include_str!("../../templates/bash/mysql_secure.sh"));
 
-        shell_exec(&format!(r#"dos2unix mysql_secure.sh && chmod +x mysql_secure.sh && ./mysql_secure.sh "{}" && rm -rf mysql_secure.sh"#,root_password));
+        shell_exec(&format!(r#"dos2unix mysql_secure.sh && chmod +x mysql_secure.sh && ./mysql_secure.sh "{root_password}" && rm -rf mysql_secure.sh"#));
 
-        shell_exec(&format!(r#"mysql --execute "ALTER USER root@'localhost' IDENTIFIED BY '{}'""#,root_password));
+        shell_exec(&format!(r#"mysql --execute "ALTER USER root@'localhost' IDENTIFIED BY '{root_password}'""#));
 
 
         file_put_contents("/root/.my.cnf",&format!(include_str!("../../templates/mysql/client.cnf"),"root",root_password));
