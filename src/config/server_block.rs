913443d092722fc7385c10cf7ebc7fc9f32a36eb
Akintomiwa Opemipo <ibnakintomiwa@gmail.com>
2025-05-02 01:32:16 +0100

    Handle PHP-FPM pool per user and domain name

--- src/config/server_block.rs
+++ src/config/server_block.rs
@@ -17,6 +17,10 @@ pub fn action(args: Args){
 
     let domain_name = args.domain;
     let user = args.user;
+
+    let php_fpm_pool_conf = format!(include_str!("../../templates/nginx/php-fpm-pool.conf"), user=user,domain_name=domain_name); 
+
+    file_put_contents(&format!("/etc/php/8.2/fpm/pool.d/{domain_name}.conf"), &php_fpm_pool_conf);
     
     let server_block = format!(include_str!("../../templates/nginx/server-block.conf"), domain_name=domain_name);
 
@@ -25,6 +29,9 @@ pub fn action(args: Args){
     shell_exec(&format!("sudo ln -s /etc/nginx/sites-available/{domain_name}.conf /etc/nginx/sites-enabled/"));
 
     shell_exec(&format!(r#"mkdir -p "/var/www/{domain_name}" && chown -R {user}:{user} "/var/www/{domain_name}""#));
+
+    shell_exec("sudo systemctl restart php8.2-fpm");
+    println!("Restarted php8.2-fpm service");
 
     shell_exec("sudo systemctl restart nginx");
     println!("Restarted nginx service");
