4de190bde3005ab10cc1c990effcad25cd1f9a10
Akintomiwa Opemipo <ibnakintomiwa@gmail.com>
2025-05-03 10:12:18 +0100

    Pack zip archive to releases directory

--- pack.sh
+++ pack.sh
@@ -1,5 +1,19 @@
 set -e
 
 APP_NAME=lemp
-APP_VERSION=`"target/release/$APP_NAME" --version | cut -d " " -f2`
-zip $HOME/Documents/$APP_NAME-v$APP_VERSION-ubuntu-24.04-amd64.zip target/release/$APP_NAME
+PWD=`pwd`
+APP_VERSION=`"./target/release/$APP_NAME" --version | cut -d " " -f2`
+ARCHIVE_PATH=$PWD/releases/$APP_NAME-v$APP_VERSION-ubuntu-24.04-amd64.zip
+
+mkdir -p $PWD/releases
+
+pushd "target/release" > /dev/null
+    zip -q "$ARCHIVE_PATH" $APP_NAME
+popd > /dev/null
+
+if [ -f "$ARCHIVE_PATH" ]; then
+    echo "Binary packed to $ARCHIVE_PATH"
+else
+    echo "Could not pack binary"
+    exit 1
+fi
