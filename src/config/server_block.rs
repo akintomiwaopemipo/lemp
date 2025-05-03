46d3a07154b3b46d5c200d19aff129e45f178e0f
Akintomiwa Opemipo <ibnakintomiwa@gmail.com>
2025-05-03 09:28:13 +0100

    Added use-certbot and add-unix-user flags

--- src/config/server_block.rs
+++ src/config/server_block.rs
@@ -1,4 +1,5 @@
-use util::{file_put_contents, shell_exec};
+use app::{mariadb::Mariadb, nginx::Nginx};
+use util::{change_unix_user_password, file_put_contents, read_and_confirm_password, shell_exec};
 
 #[derive(clap::Args)]
 pub struct Args{
@@ -6,7 +7,16 @@ pub struct Args{
     user: String,
 
     #[arg(short, long)]
-    domain: String
+    password: Option<String>,
+
+    #[arg(short, long)]
+    domain: String,
+
+    #[arg(long)]
+    add_unix_user: bool,
+
+    #[arg(long)]
+    use_certbot: bool
 
 }
 
@@ -18,11 +28,51 @@ pub fn action(args: Args){
     let domain_name = args.domain;
     let user = args.user;
 
+    if args.add_unix_user{
+        
+        let password = if args.password.is_some(){
+            args.password.unwrap()
+        }else{
+            read_and_confirm_password(&format!("Password for {user}"))
+        };
+
+        println!();
+
+        shell_exec(&format!(r#"adduser --gecos "" --disabled-password {user}"#));
+
+        change_unix_user_password(&user, &password);
+
+        println!("Created user account: {user}");
+
+        println!();
+
+        shell_exec(&format!("chmod +x /home/{user}"));
+
+        println!();
+
+        shell_exec(&format!("mkdir -p /home/{user}/public_html"));
+
+        file_put_contents(&format!("/home/{user}/public_html/error.log"),"");
+
+        Mariadb::configure(&user, &password, None);
+
+        println!();
+
+    }
+
+
+
     let php_fpm_pool_conf = format!(include_str!("../../templates/nginx/php-fpm-pool.conf"), user=user,domain_name=domain_name); 
 
     file_put_contents(&format!("/etc/php/8.2/fpm/pool.d/{domain_name}.conf"), &php_fpm_pool_conf);
+
+    let root_directory = if args.add_unix_user{
+        format!("/home/{user}/public_html")
+    }else{
+        format!("/var/www/{domain_name}")
+    };
     
-    let server_block = format!(include_str!("../../templates/nginx/server-block.conf"), domain_name=domain_name);
+    let server_block = format!(include_str!("../../templates/nginx/server-block.conf"), domain_name=domain_name, root_directory=root_directory);
 
     file_put_contents(&format!("/etc/nginx/sites-available/{domain_name}.conf"), &server_block);
 
@@ -33,8 +83,15 @@ pub fn action(args: Args){
     shell_exec("sudo systemctl restart php8.2-fpm");
     println!("Restarted php8.2-fpm service");
 
-    shell_exec("sudo systemctl restart nginx");
-    println!("Restarted nginx service");
+    Nginx::restart();
+
+
+    if args.use_certbot{
+        shell_exec(&format!(r#"certbot -d "{domain_name}" --nginx --non-interactive --agree-tos --register-unsafely-without-email --no-redirect"#));
+
+        Nginx::restart();
+    }
+
 }
 
 
